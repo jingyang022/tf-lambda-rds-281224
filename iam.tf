4bc52190912860fbb45f78b441ac85543836bd22
jingyang022 <jingyang022@yahoo.com.sg>
2024-12-29 10:37:29 +0800

    added files

--- iam.tf
+++ iam.tf
@@ -1,3 +1,13 @@
+data "aws_iam_policy" "lambda_full_access" {
+  name = "AWSLambda_FullAccess"
+}
+
+resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
+  name = "lambda_policy_attachment"
+  roles = aws_iam_role.lambda_exec_role.name
+  policy_arn = data.aws_iam_policy.lambda_full_access.arn
+}
+
 resource "aws_iam_role" "lambda_exec_role" {
  name = "lambda_execution_role"
   assume_role_policy = jsonencode({
@@ -18,3 +28,11 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
  role       = aws_iam_role.lambda_exec_role.name
  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+/* resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.func.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.bucket.arn
+} */
--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+#############################################################
+# Lambda Function
+#############################################################
+
 # Define an archive_file datasource that creates the lambda archive
 data "archive_file" "lambda" {
  type        = "zip"
@@ -6,14 +10,6 @@ data "archive_file" "lambda" {
  output_path = "lambda_function.zip"
 }
 
-/* resource "aws_lambda_permission" "allow_bucket" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.func.arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.bucket.arn
-} */
-
 resource "aws_lambda_function" "func" {
  function_name = "yap-moviesdb-api"
  role          = aws_iam_role.lambda_exec_role.arn
@@ -21,10 +17,21 @@ resource "aws_lambda_function" "func" {
  runtime       = "python3.8"
  filename      = data.archive_file.lambda.output_path
 
+# VPC Configuration
  vpc_config {
     # Choose all the db subnets same as RDS
     subnet_ids         = data.aws_subnets.db.ids
     security_group_ids = [aws_security_group.allow_Lambda-RDS.id]
+  }
+
+  # Environment Variables
+  environment {
+    variables = {
+       	DB_NAME = "",
+        USERNAME = "",
+        PASSWORD = "",
+        RDS_ENDPOINT = ""
+    }
   }
 }
 
